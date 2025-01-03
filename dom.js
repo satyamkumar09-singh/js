1b3a7b5cd0178925cbc0aa9cadf314ddeff206ae
satyamkumar9-singh <satyamkumar2559@gmail.com>
2025-01-03 18:54:27 +0530

    done

---
+++ dom.js
@@ -0,0 +1,9 @@
+let heading1 =document.getElementById('headcontent');
+heading1.textContent ="cybrom"
+heading1.innerHTML ="<i>Technical institute</i>"
+heading1.style.color = "red"
+heading1.style.backgroundColor ="yellow"
+heading1.style.padding="20px"
+
+let myimg =document.getElementById('delhiimg');
+myimg.src ="jaipur image.jpg"
