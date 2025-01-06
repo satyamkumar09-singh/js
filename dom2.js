fbf2909da99d28e0843d42bf436cf48da42b7362
satyamkumar9-singh <satyamkumar2559@gmail.com>
2025-01-06 19:37:59 +0530

    done

--- dom2.js
+++ dom2.js
@@ -4,3 +4,9 @@ head.textContent = "Welcome to My Website"
 let bd = document.body
 
 bd.append(head)
+bd.setAttribute('bgcolor','red')
+
+head.setAttribute('align','center')
+
+let as =document.getElementById('as')
+as.removeAttribute('align')
---
+++ dom4.js
@@ -0,0 +1,8 @@
+function fun2(){
+   let image = document.getElementById('image')
+    image.src="jaipur image.jpeg"
+}
+
+function red(colorr){
+    document.body.style.backgroundcolor="colorr"
+}
