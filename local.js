4f8406e4fc7c7a3e5b299a71e59bd93d2b3230f2
satyamkumar9-singh <satyamkumar2559@gmail.com>
2025-02-10 18:56:32 +0530

    done

--- local.js
+++ local.js
@@ -10,3 +10,28 @@
 //2 getItem()      ->data retrive in localstorage
 //3 removeItem()   ->particular data remove in localstoarge
 //4 clear()        ->all data clear in localstorage
+
+
+const multi=()=>{
+    let userdata={
+        name:document.querySelector('#name').value,
+        email:document.querySelector('#email').value,
+        password:document.querySelector('#password').value
+
+    }
+    localStorage.setItem("userdata",JSON.stringify(userdata))
+}
+
+let data=JSON.parse(localStorage.getItem('userdata'))
+console.log(data);
+
+function loginform(){
+    let logindata={
+        loginemail:document.querySelector('#loginemail').value,
+        loginpassword:document.querySelector('#loginpassword').value,
+    }
+    if(data.email!=logindata.loginemail ||data.password != logindata.loginpassword){
+        alert("user not found")
+            return false
+    }
+}
